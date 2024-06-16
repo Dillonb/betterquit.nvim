b81ccb52c3b7155d1605f60f36cca6d3fdd3192e
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-06-16 13:37:19 -0700

    Exclude more buffers from count. Use quitall() when one buffer is left to catch plugin buffers.

--- lua/betterquit.lua
+++ lua/betterquit.lua
@@ -2,14 +2,25 @@ local betterquit = {}
 
 local function buffer_count()
   local bufs = vim.api.nvim_list_bufs()
-  count = 0
+  local count = 0
 
   for _, buf in ipairs(bufs) do
-    if vim.api.nvim_buf_is_loaded(buf) then
+    local bufname = vim.api.nvim_buf_get_name(buf)
+    local readonly = vim.api.nvim_buf_get_option(buf, 'readonly')
+    local listed = vim.api.nvim_buf_get_option(buf, 'buflisted')
+    local buftype = vim.api.nvim_buf_get_option(buf, 'buftype')
+
+    local buf_is_file = vim.api.nvim_buf_is_loaded(buf)
+      and (bufname ~= '')
+      and (not readonly)
+      and (listed)
+      and (buftype ~= 'terminal')
+      and (buftype ~= 'quickfix')
+
+    if buf_is_file then
       count = count + 1
     end
   end
-
   return count
 end
 
@@ -21,7 +32,9 @@ local function close_buffer_or_quit(opts)
   if opts.bang then -- Close everything without saving, just like :q! should
     vim.cmd.quit { bang = opts.bang, mods = { silent = true } }
   elseif num_buffers <= 1 then -- This is the last buffer.
-    vim.cmd.quit() -- default quit behavior, will warn if there are unsaved changes in the last buffer
+    -- default quit behavior, close all buffers. This is to catch sidebar/quickfix/other plugin buffers
+    -- Will warn if there are unsaved changes in the last buffer
+    vim.cmd.quitall()
   else -- just close this buffer
     vim.cmd [[
       BufferClose
